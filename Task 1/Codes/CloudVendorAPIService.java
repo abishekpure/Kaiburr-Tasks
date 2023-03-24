72b712c3c35c6fde8ca6575beb0606edd5bc9541
abishekpure <124038118+abishekpure@users.noreply.github.com>
2023-03-24 21:26:40 +0300

    Add files via upload

---
+++ Task 1/Codes/CloudVendorAPIService.java
@@ -0,0 +1,41 @@
+package com.example.restdemo.controller;
+
+import com.example.restdemo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService
+{
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+
+    public CloudVendor getCloudVendorDetails(String vendorId)
+    {
+        return cloudVendor;
+    }
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor=cloudVendor;
+        return "Cloud Vendore Created Successfully";
+
+
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor=cloudVendor;
+        return "Cloud Vendor updated successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId)
+    {
+        this.cloudVendor=null;
+        return "Cloud Vendor deleted successfully";
+
+    }
+
+}
